5b0c43eae1d70004399f53ab46b870365ccbd0fb
Alley Manalio <amanalio@ufl.edu>
2016-08-03 20:21:12 -0400

    Add Slick image slider jQuery plugin on index

--- js/main.js
+++ js/main.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
         // Instantiate MixItUp:
         $('#Container').mixItUp();
     });
+
+    $('.image-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
 });

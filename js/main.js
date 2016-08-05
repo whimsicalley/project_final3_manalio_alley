1582db7a1fcb9574e7495994e89064ce5f01f1dd
Alley Manalio <amanalio@ufl.edu>
2016-08-04 20:56:12 -0400

    Add custom jQuery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
+    // Initiates MixItUp, the jQuery plugin on the menu page.
     $(function () {
-        // Instantiate MixItUp:
         $('#Container').mixItUp();
     });
 
+    // Initiates Slick, the image slider jQuery plugin on the Index page
     $('.image-slider').slick({
         dots: true,
         infinite: true,
@@ -13,5 +14,11 @@ $(document).ready(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000
+    });
+
+    // If you click on the photo of Gusteau on the About page, a photo of Remy will appear below Gusteau's previous sibling, in this case, the h3 tag "History"
+    var photo = $('.photo');
+    photo.on('click', function () {
+        photo.next().html('<img src="img/remy.jpg">');
     });
 });
